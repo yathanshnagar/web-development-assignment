ce2c551ab6ddeb3ad85a6c7b5380cdd00303e146
Akshay Gandhi <akshayg@lambdatest.com>
2024-03-16 01:18:13 +0530

    test

--- rewrite2.js
+++ rewrite2.js
@@ -59,10 +59,17 @@ table.addEventListener('change', function(event) {
       // Get all checkboxes
       const checkboxes = document.querySelectorAll('.selected-checkbox');
       let selectedCount = 0;
+      let firstChar = -1;
+      let secondChar = -1;
       // Count the number of selected checkboxes
-      checkboxes.forEach(function(checkbox) {
+      checkboxes.forEach(function(checkbox, index) {
         if (checkbox.checked) {
           selectedCount++;
+          if (firstChar == -1) {
+            firstChar = index
+          } else {
+            secondChar = index
+          }
         }
       });
       // Check if more than two checkboxes are selected
@@ -71,6 +78,113 @@ table.addEventListener('change', function(event) {
         event.target.checked = false;
         return;
       }
+
+      document.getElementById('left-strength').innerText = "";
+      document.getElementById('left-speed').innerText = "";
+      document.getElementById('left-skill').innerText = "";
+      document.getElementById('left-fear_factor').innerText = "";
+      document.getElementById('left-power').innerText = "";
+      document.getElementById('left-intelligence').innerText = "";
+      document.getElementById('left-wealth').innerText = "";
+      
+      document.getElementById('right-strength').innerText = "";
+      document.getElementById('right-speed').innerText = "";
+      document.getElementById('right-skill').innerText = "";
+      document.getElementById('right-fear_factor').innerText = "";
+      document.getElementById('right-power').innerText = "";
+      document.getElementById('right-intelligence').innerText = "";
+      document.getElementById('right-wealth').innerText = "";
+
+
+      document.getElementById('left-char').innerText = ""
+      document.getElementById('right-char').innerText = ""
+      
+      document.getElementById('left-char-image').src = "";
+      document.getElementById('right-char-image').src = ""
+
+
+      if (firstChar != -1) {
+        document.getElementById('left-char-image').src = "./" + characterList[firstChar].image_url;
+        document.getElementById('left-char').innerText = characterList[firstChar].name
+      }
+      
+      if (secondChar != -1) {
+        let wonByOne = 0;
+        let wonByTwo = 0;
+        document.getElementById('right-char-image').src = "./" + characterList[secondChar].image_url;
+        document.getElementById('right-char').innerText = characterList[secondChar].name;
+
+        if (characterList[firstChar].strength > characterList[secondChar].strength) {
+            document.getElementById('left-strength').innerHTML = "\u2713"
+            wonByOne += 1
+        } else {
+            document.getElementById('right-strength').innerHTML = "\u2713"
+            wonByTwo += 1
+        }
+        
+        if (characterList[firstChar].speed > characterList[secondChar].speed) {
+            document.getElementById('left-speed').innerHTML = "\u2713"
+            wonByOne += 1
+        } else {
+            document.getElementById('right-speed').innerHTML = "\u2713"
+            wonByTwo += 1
+        }
+
+        if (characterList[firstChar].skill > characterList[secondChar].skill) {
+            document.getElementById('left-skill').innerHTML = "\u2713"
+            wonByOne += 1
+        } else {
+            document.getElementById('right-skill').innerHTML = "\u2713"
+            wonByTwo += 1
+        }
+
+        if (characterList[firstChar].fear_factor > characterList[secondChar].fear_factor) {
+            document.getElementById('left-fear_factor').innerHTML = "\u2713"
+            wonByOne += 1
+        } else {
+            document.getElementById('right-fear_factor').innerHTML = "\u2713"
+            wonByTwo += 1
+
+        }
+        
+        if (characterList[firstChar].power > characterList[secondChar].power) {
+            wonByOne += 1
+            document.getElementById('left-power').innerHTML = "\u2713"
+        } else {
+            document.getElementById('right-power').innerHTML = "\u2713"
+            wonByTwo += 1
+
+        }
+        
+        if (characterList[firstChar].intelligence > characterList[secondChar].intelligence) {
+            document.getElementById('left-intelligence').innerHTML = "\u2713"
+            wonByOne += 1
+        } else {
+            document.getElementById('right-intelligence').innerHTML = "\u2713"
+            wonByTwo += 1
+
+        }
+        
+        if (characterList[firstChar].wealth > characterList[secondChar].wealth) {
+            wonByOne += 1
+            document.getElementById('left-wealth').innerHTML = "\u2713"
+        } else {
+            document.getElementById('right-wealth').innerHTML = "\u2713"
+            wonByTwo += 1
+
+        }
+
+        if (wonByOne > wonByTwo) {
+          document.getElementById('char-card1').style.backgroundColor = "green";
+          document.getElementById('char-card2').style.backgroundColor = "red";
+        } else {
+          document.getElementById('char-card1').style.backgroundColor = "red";
+          document.getElementById('char-card2').style.backgroundColor = "green";
+        }
+
+        console.log("wonByOne", wonByOne, "wonByTwo", wonByTwo)
+      }
+
       // Get the row of the changed checkbox
       const row = event.target.closest('tr');
       // Toggle the "selected" class based on checkbox state
